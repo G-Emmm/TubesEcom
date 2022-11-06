f53c0b94c56255b734439393e7f05db4e6eadde6
Nisa Zahir <nisa24.bz@student.uns.ac.id>
2022-11-06 19:32:26 +0700

    create setting table

---
+++ Presensi/database/migrations/2022_11_06_122206_create_setting_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setting', function (Blueprint $table) {
+            $table->bigIncrements('id_setting');
+            $table->unsignedBigInteger('id_opd');
+            $table->double('batas_jarak_presensi');
+
+            $table->foreign('id_opd')->references('id_opd')->on('opd');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setting');
+    }
+};
